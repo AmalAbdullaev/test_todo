d6730336146bce9beec7eb302fa4d9f83288f32e
Amal Abdullaev <>
2021-02-10 13:33:24 +0300

    refactoring

--- demo.js
+++ demo.js
@@ -4,64 +4,54 @@ var doneList = document.getElementById("donelist");
 var todo = [];
 var todone = [];
 
-
 var temp;
 var count = 0, countDone = 0, x = 0, y = 0;
 var flag = true;
 
+init()
+
+function save_data(name, data, isJson) {
+    var str = data;
+    isJson ? str = JSON.stringify(str) : null;
+    localStorage.setItem(name, str);
+}
+
+function get_data(name, isJson) {
+    var data = localStorage.getItem(name);
+    return isJson ? JSON.parse(data) || [] : data;
+}
+
 function save_count() {
-    var localCount = count;
-    var localCountDone = countDone;
-    localStorage.setItem("count", localCount);
-    localStorage.setItem("countDone", localCountDone);
+    save_data("count", count, false);
+    save_data("countDone", countDone, false);
+}
+
+function save_list() {
+    save_data("todo", todo, true);
+    save_data("todone", todone, true);
 }
 
 function get_count() {
-    var localCount = localStorage.getItem("count");
-    var localCountDone = localStorage.getItem("countDone");
-    count = localCount;
-    countDone = localCountDone;
-}
-
-
-function save_list() {
-    var str = JSON.stringify(todo);
-    var str1 = JSON.stringify(todone);
-    localStorage.setItem("todo", str);
-    localStorage.setItem("todone", str1);
-
+    count = get_data("count", false);
+    countDone = get_data("countDone", false);
 }
 
 function get_list() {
-    var str = localStorage.getItem("todo");
-    var str1 = localStorage.getItem("todone");
-
-    todo = JSON.parse(str);
-    todone = JSON.parse(str1);
-    if (!todo) {
-        todo = [];
-    }
-    if (!todone) {
-        todone = [];
-    }
-
-    console.log(todo);
-    console.log(todone);
-
-}
-get_count();
-get_list();
-var todoCount = count;
-var todoneCount = countDone;
-
-while (todoCount > 0) {
-    additem_todo('todo');
-    --todoCount;
+    todo = get_data("todo", true);
+    todone = get_data("todone", true);
 }
 
-while (todoneCount > 0) {
-    additem_todo('todone');
-    --todoneCount;
+function init() {
+    get_count();
+    get_list();
+    createList(count, 'todo');
+    createList(countDone, 'todone');
+}
+
+function createList(count, name) {
+    for (var c = 0; c < count; c++) {
+        additem_todo(name);
+    } 
 }
 
 function additem_whenenter(e) {
@@ -92,8 +82,6 @@ function additem_todo(flag) {
         myList.appendChild(item);
         x++;
         document.getElementById("noitem").style.display = "none";
-
-
     }
     else if (flag == 'todone') {
         check.checked = true;
@@ -102,8 +90,6 @@ function additem_todo(flag) {
         doneList.appendChild(item);
         y++;
         document.getElementById("doneitem").style.display = "none";
-
-
     }
     else {
         var itemContent = document.getElementById("itemname");
@@ -119,7 +105,6 @@ function additem_todo(flag) {
 
 
     check.addEventListener("change", function () {
-
         var doneItem = this.parentElement;
         temp = doneItem.childNodes[2].innerHTML;
         if (myList.id == this.parentElement.parentElement.id) {
@@ -129,10 +114,7 @@ function additem_todo(flag) {
             count--;
             countDone++;
             todone.push(temp);
-            for (var i = 0; i < todo.length; i++) {
-                if (temp == todo[i])
-                    todo.splice(i, 1);
-            }
+            todo = remove_item(temp, todo)
         }
         else {
             doneList.removeChild(doneItem);
@@ -141,125 +123,111 @@ function additem_todo(flag) {
             count++;
             countDone--;
             todo.push(temp);
-            for (var i = 0; i < todone.length; i++) {
-                if (temp == todone[i])
-                    todone.splice(i, 1);
-            }
+            todone = remove_item(temp, todone);
         }
         save_list();
         save_count();
         display_title();
+    })
 
-        spanimg.addEventListener("click", function () {
+    spanimg.addEventListener("click", function () {
 
-            var removeItem = this.parentElement;
-            temp = removeItem.childNodes[2].innerHTML;
+        var removeItem = this.parentElement;
+        temp = removeItem.childNodes[2].innerHTML;
 
-            if (myList.id == this.parentElement.parentElement.id) {
-                myList.removeChild(removeItem);
-                count--;
-                for (var i = 0; i < todo.length; i++) {
-                    if (temp == todo[i])
-                        todo.splice(i, 1);
-                }
-
-            }
-            else {
-                doneList.removeChild(removeItem);
-                countDone--;
-                for (var i = 0; i < todone.length; i++) {
-                    if (temp = todone[i])
-                        todone.splice(i, 1);
-                }
-            }
-            save_list();
-            save_count();
-            display_title();
-
-        });
-
-        span.addEventListener("dblclick", function () {
-
-            var data = this.innerHTML;
-            var parent = this.parentElement;
-            span.innerHTML = "";
-            var form = document.createElement("span");
-            var text = document.createElement("input");
-            var ok = document.createElement("button");
-            var cancel = document.createElement("button");
-
-            text.value = data;
-            ok.innerHTML = "OK";
-            cancel.innerHTML = "Cancel";
-
-            form.appendChild(text);
-            form.appendChild(ok);
-            form.appendChild(cancel);
-            span.appendChild(form);
-
-            ok.addEventListener("click", function () {
-                span.removeChild(form);
-
-                for (var i = 0; i < todo.length; i++) {
-                    if (data == todo[i]) {
-                        todo[i] = text.value;
-                    }
-
-                }
-
-                for (var i = 0; i < todone.length; i++) {
-                    if (data == todone[i]) {
-                        todone[i] = text.value;
-                    }
-                }
-                save_list();
-                data = text.value;
-                span.innerHTML = data;
-                parent.appendChild(span);
-            });
-
-            cancel.addEventListener("click", function () {
-                span.removeChild(form);
-                span.innerHTML = data;
-                parent.appendChild(span);
-            });
-        });
-    }
-
-    function display_title() {
-
-        if (count == 0) {
-            document.getElementById("noitem").style.display = "block";
-        }
-        if (count == 1) {
-            document.getElementById("noitem").style.display = "none";
-        }
-        if (countDone == 0) {
-            document.getElementById("doneitem").style.display = "block";
-        }
-        if (countDone == 1) {
-            document.getElementById("doneitem").style.display = "none";
-        }
-
-    }
-    function remove_list(id) {
-        var root = document.getElementById(id);
-        while (root.firstChild) {
-            root.removeChild(document.getElementById(id).firstChild);
-
-        }
-
-
-        if (myList.id == id) {
-            todo = [];
-            count = 0;
+        if (myList.id == this.parentElement.parentElement.id) {
+            myList.removeChild(removeItem);
+            count--;
+            todo = remove_item(temp, todo);
         }
         else {
-            todone = [];
-            countDone = 0;
+            doneList.removeChild(removeItem);
+            countDone--;
+            todone = remove_item(temp, todone);
         }
         save_list();
         save_count();
         display_title();
+    })
 
+    span.addEventListener("dblclick", function () {
+        var data = this.innerHTML;
+        var parent = this.parentElement;
+        span.innerHTML = "";
+        var form = document.createElement("span");
+        var text = document.createElement("input");
+        var ok = document.createElement("button");
+        var cancel = document.createElement("button");
+
+        text.value = data;
+        ok.innerHTML = "OK";
+        cancel.innerHTML = "Cancel";
+
+        form.appendChild(text);
+        form.appendChild(ok);
+        form.appendChild(cancel);
+        span.appendChild(form);
+
+        ok.addEventListener("click", function () {
+            span.removeChild(form);
+            todo = change_item(data, text.value, todo);
+            todone = change_item(data, text.value, todone);
+            console.log(todo)
+            save_list();
+            data = text.value;
+            span.innerHTML = data;
+            parent.appendChild(span);
+        });
+
+        cancel.addEventListener("click", function () {
+            span.removeChild(form);
+            span.innerHTML = data;
+            parent.appendChild(span);
+        });
+    });
+}
+
+function change_item(data, value, array) {
+    const index = array.indexOf(data);
+    if (array[index]) {
+        array[index] = value;
     }
+    return array;
+}
+
+function remove_item(temp, array) {
+    const index = array.indexOf(temp);
+    if (index > -1) {
+        array.splice(index, 1);
+    }
+    return array;
+}
+
+function display_title() {
+    display_element("noitem", count);
+    display_element("doneitem", countDone);
+}
+
+function display_element(elem, data) {
+    var state = data > 0 ? "none" : "block";
+    document.getElementById(elem).style.display = state;
+}
+
+function remove_list(id) {
+    var root = document.getElementById(id);
+    while (root.firstChild) {
+        root.removeChild(document.getElementById(id).firstChild);
+    }
+
+    if (myList.id == id) {
+        todo = [];
+        count = 0;
+    }
+    else {
+        todone = [];
+        countDone = 0;
+    }
+    save_list();
+    save_count();
+    display_title();
+}
